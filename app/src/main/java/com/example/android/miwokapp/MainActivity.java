f7faa789da15f1468738c7609f7c2c84cffffde3
Konstantinos Tsekos <konstantinos.tsekos@gmail.com>
2018-02-11 23:17:28 +0200

    Added intent for each activity

--- app/src/main/java/com/example/android/miwokapp/MainActivity.java
+++ app/src/main/java/com/example/android/miwokapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.miwokapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+    public void openFamilyList(View view){
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+
+    public void openColorsList(View view){
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
+    }
+
+    public void openPhrasesList(View view){
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
     }
 }
